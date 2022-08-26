443ce53f8f7f76412617fab8554db0b41ed032d0
long.nguyen <nguyennhulong210822@gmail.com>
2022-08-26 08:37:10 +0700

    js

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-html5
+function kick() {
+    var kick = document.getElementById("kick")
+    kick.play();
+}
+
+function clap() {
+    var clap = document.getElementById("clap")
+    clap.play();
+}
+
+function hihat() {
+    var hihat = document.getElementById("hihat")
+    hihat.play();
+}
+
+function openhat() {
+    var openhat = document.getElementById("openhat")
+    openhat.play();
+}
+
+function tink() {
+    var tink = document.getElementById("tink")
+    tink.play();
+}
+
+function snare() {
+    var snare = document.getElementById("snare")
+    snare.play();
+}
+
+function tom(params) {
+    var tom = document.getElementById("tom")
+    tom.play();
+}
+
+function ride(params) {
+    var ride = document.getElementById("ride")
+    ride.play();
+}
+
+function boom(params) {
+    var boom = document.getElementById("boom")
+    boom.play();
+}
